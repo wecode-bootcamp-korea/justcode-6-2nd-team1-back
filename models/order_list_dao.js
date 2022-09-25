057b94cf24b06d26105aa4ca3fca43532037edc2
전준영 <wnsdud9476@DESKTOP-2VICEL1.localdomain>
2022-09-25 22:38:42 +0900

    [ADD] 주문내역 추가

--- models/order_list_dao.js
+++ models/order_list_dao.js
@@ -2,7 +2,28 @@ const { myDataSource } = require("./common");
 
 const getOrderListByUserId = async (userId) => {
   return await myDataSource.query(
-    `
-    `
+    `SELECT 
+    orders.id as orderId,
+    beverages.beverage_name,
+    beverages.beverage_image,
+    orders.amount,
+    orders.total_price,
+    orders.order_status_id,
+    orders.cold, 
+    orders.sugar, 
+    orders.ice,
+      json_arrayagg(
+          json_object(
+          "topping_id",topping_id,
+          "amount",topping_order.amount)) as toppings
+          FROM orders
+          JOIN beverages ON orders.beverage_id = beverages.id
+          RIGHT JOIN topping_order ON orders.id = topping_order.order_id
+          WHERE user_id = ? AND order_status_id > 2
+          GROUP BY orders.id;
+    `,
+    [userId]
   );
 };
+
+module.exports = { getOrderListByUserId };
--- routes/user_router.js
+++ routes/user_router.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const userController = require("../controllers/user_controller");
 const orderListController = require("../controllers/order_list_controller");
-const validateToken = require("../middlewares/validate_token");
+const { validateToken } = require("../middlewares/validate_token");
 
 const router = express.Router();
 
